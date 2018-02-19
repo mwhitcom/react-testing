b3f3181ba5159fa7508a0b0be4e2f2d4a051ce98
Matt Whitcomb <mattwhitcomb@Matts-MacBook-Pro.local>
2018-02-19 15:32:35 -0800

    testing github

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -3,8 +3,6 @@ import { Button } from 'react-bootstrap';
 import Gift from './Gift';
 import { max_number } from '../helper';
 
-// hello
-
 class App extends Component {
   constructor(){
     super();
